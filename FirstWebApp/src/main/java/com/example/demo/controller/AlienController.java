49c513c190e8abccac3ec34c26560a3a86caf65c
kiran <kiran1990sai@gmail.com>
2022-08-03 20:37:10 -0300

    First release

---
+++ FirstWebApp/src/main/java/com/example/demo/controller/AlienController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AlienController {
+	
+	public void home() {
+		
+	}
+	
+	
+
+}
